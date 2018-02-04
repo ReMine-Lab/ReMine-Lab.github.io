057e80a91a89cd36e25c5afa2494e704ee0e32b4
cuiyungao <gcyydxf@gmail.com>
2018-02-04 17:35:24 +0800

    update contact

---
+++ js/contact_postmail.js
@@ -0,0 +1,44 @@
+//update this with your $form selector
+var form_id = "contactForm";
+
+var data = {
+    "access_token": "er956uxw694dn2rpecqpvhet"
+};
+
+function onSuccess() {
+    // remove this to avoid redirect
+    window.location = window.location.pathname + "?message=Email+Successfully+Sent%21&isError=0";
+}
+
+function onError(error) {
+    // remove this to avoid redirect
+    window.location = window.location.pathname + "?message=Email+could+not+be+sent.&isError=1";
+}
+
+var sendButton = $("#" + form_id + " [name='sendMessageButton']");
+
+function send() {
+    sendButton.val('Sending…');
+    sendButton.prop('disabled',true);
+
+    var subject = $("#" + form_id + " [name='name']").val();
+    var email = $("#" + form_id + " [name='email']").val();
+    var message = $("#" + form_id + " [name='message']").val();
+    data['subject'] = subject;
+    data['email'] = email;
+    data['text'] = message;
+
+    $.post('https://postmail.invotes.com/send',
+        data,
+        onSuccess
+    ).fail(onError);
+
+    return false;
+}
+
+sendButton.on('click', send);
+
+var $form = $("#" + form_id);
+$form.submit(function( event ) {
+    event.preventDefault();
+});
